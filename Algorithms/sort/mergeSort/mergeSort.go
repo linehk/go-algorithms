211f6e97c94498a8e4f88e6ee7ff4a4a188090c4
sulinehk <sulinehk@gmail.com>
2018-12-13 19:15:47 +0800

    Algorithms/sort: fix mergeSort

--- Algorithms/sort/mergeSort/mergeSort.go
+++ Algorithms/sort/mergeSort/mergeSort.go
@@ -1,9 +1,19 @@
 package mergeSort
 
 func MergeSort(elements []int) {
+	l := len(elements)
+	aux := make([]int, l)
+	sort(elements, aux, 0, l-1)
 }
 
-func sort() {
+func sort(elements []int, aux []int, lo, hi int) {
+	if hi <= lo {
+		return
+	}
+	mid := lo + (hi-lo)/2
+	sort(elements, aux, lo, mid)
+	sort(elements, aux, mid+1, hi)
+	merge(elements, aux, lo, mid, hi)
 }
 
 func merge(elements []int, aux []int, lo, mid, hi int) {
@@ -21,12 +31,12 @@ func merge(elements []int, aux []int, lo, mid, hi int) {
 		} else if j > hi {
 			elements[k] = aux[i]
 			i++
-		} else if aux[i] < aux[j] {
-			elements[k] = aux[i]
-			i++
 		} else if aux[j] < aux[i] {
 			elements[k] = aux[j]
 			j++
+		} else {
+			elements[k] = aux[i]
+			i++
 		}
 	}
 }
--- Algorithms/sort/mergeSort/mergeSort_test.go
+++ Algorithms/sort/mergeSort/mergeSort_test.go
@@ -8,7 +8,9 @@ func TestMergeSort(t *testing.T) {
 	tests := []struct {
 		input []int
 	}{
-		{[]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}},
+		{[]int{4, 3, 2, 1, 0}},
+		{[]int{2, 1, 0}},
+		{[]int{3, 2, 1, 0}},
 	}
 	for i, tt := range tests {
 		MergeSort(tt.input)
@@ -16,7 +18,6 @@ func TestMergeSort(t *testing.T) {
 			t.Errorf("%v. got %v, want %v", i, got, true)
 		}
 	}
-
 }
 
 func isSorted(elements []int) bool {
@@ -27,11 +28,4 @@ func isSorted(elements []int) bool {
 		}
 	}
 	return true
-}
-
-func TestMerge(t *testing.T) {
-	input := []int{3, 4, 0, 1}
-	aux := make([]int, len(input))
-	merge(input, aux, 0, 2, 4)
-	t.Log(input)
 }
