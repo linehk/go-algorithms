49879a4d3249e637c889e40060629f64174a2450
sulinehk <sulinehk@gmail.com>
2018-12-13 09:46:26 +0800

    Algorithms/sort/insertionSort: add insertionSortX

--- Algorithms/sort/insertionSort/insertionSort.go
+++ Algorithms/sort/insertionSort/insertionSort.go
@@ -10,3 +10,29 @@ func InsertionSort(elements []int) {
 		}
 	}
 }
+
+func InsertSortX(elements []int) {
+	l := len(elements)
+
+	// put smallest element in position to serve as sentinel
+	exchanges := 0
+	for i := l - 1; i > 0; i-- {
+		if elements[i] < elements[i-1] {
+			elements[i], elements[i-1] = elements[i-1], elements[i]
+		}
+		exchanges++
+	}
+	if exchanges == 0 {
+		return
+	}
+
+	// insertion sort with half-exchanges
+	for i := 2; i < l; i++ {
+		j := i
+		v := elements[i]
+		for ; v < elements[j-1]; j-- {
+			elements[j] = elements[j-1]
+		}
+		elements[j] = v
+	}
+}
--- Algorithms/sort/insertionSort/insertionSort_test.go
+++ Algorithms/sort/insertionSort/insertionSort_test.go
@@ -20,10 +20,26 @@ func TestInsertionSort(t *testing.T) {
 
 }
 
+func TestInsertionSortX(t *testing.T) {
+	tests := []struct {
+		input []int
+	}{
+		{[]int{0}},
+		{[]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}},
+	}
+	for i, tt := range tests {
+		InsertSortX(tt.input)
+		if got := isSorted(tt.input); !got {
+			t.Errorf("%v. got %v, want %v", i, got, true)
+		}
+	}
+
+}
+
 func isSorted(elements []int) bool {
 	l := len(elements)
-	for i := 0; i < l-1; i++ {
-		if elements[i] > elements[i+1] {
+	for i := 1; i < l; i++ {
+		if elements[i] < elements[i-1] {
 			return false
 		}
 	}
