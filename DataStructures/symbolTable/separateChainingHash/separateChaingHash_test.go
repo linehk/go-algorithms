e399dbcc791f8070937300abca2c2e92d2f7b349
sulinehk <sulinehk@gmail.com>
2018-12-28 22:52:57 +0800

    DataStructures/symbolTabl: add separateChainingHash

--- DataStructures/symbolTable/separateChainingHash/separateChaingHash_test.go
+++ DataStructures/symbolTable/separateChainingHash/separateChaingHash_test.go
@@ -1,1 +1,133 @@
 package separateChainingHash
+
+import (
+	"testing"
+)
+
+func TestContains(t *testing.T) {
+	tests := []struct {
+		input map[string]int
+		key   string
+		want  bool
+	}{
+		{map[string]int{"a": 0}, "a", true},
+		{map[string]int{"a": 0}, "b", false},
+		{map[string]int{"abc": 3, "bced": 4, "cedff": 5}, "bced", true},
+	}
+	for i, tt := range tests {
+		s := New(len(tt.input))
+		for k, v := range tt.input {
+			s.Put(k, v)
+		}
+		if got := s.Contains(tt.key); got != tt.want {
+			t.Errorf("%v. got %v, want %v", i, got, tt.want)
+		}
+	}
+}
+
+func TestGet(t *testing.T) {
+	tests := []struct {
+		input map[string]int
+		key   string
+		want  int
+	}{
+		{map[string]int{"a": 0}, "a", 0},
+		{map[string]int{"a": 0, "b": 1, "c": 2}, "b", 1},
+		{map[string]int{"a": 0, "b": 1, "c": 2}, "d", 0},
+	}
+	for i, tt := range tests {
+		s := New(len(tt.input))
+		for k, v := range tt.input {
+			s.Put(k, v)
+		}
+
+		got, _ := s.Get(tt.key)
+
+		if got != tt.want {
+			t.Errorf("%v. got %v, want %v", i, got, tt.want)
+		}
+	}
+}
+
+func TestPut(t *testing.T) {
+	tests := []struct {
+		input map[string]int
+		key   string
+		value int
+		want  int
+	}{
+		{map[string]int{"a": 0}, "b", 1, 1},
+		{map[string]int{"a": 0, "b": 1, "c": 2}, "c", 2, 2},
+	}
+	for i, tt := range tests {
+		s := New(len(tt.input))
+		for k, v := range tt.input {
+			s.Put(k, v)
+		}
+
+		s.Put(tt.key, tt.value)
+
+		got, err := s.Get(tt.key)
+		if err != nil {
+			t.Error(err)
+		}
+		if got != tt.want {
+			t.Errorf("%v. got %v, want %v", i, got, tt.want)
+		}
+	}
+}
+
+func TestDelete(t *testing.T) {
+	tests := []struct {
+		input     map[string]int
+		deleteKey string
+		getKey    string
+		want      int
+	}{
+		{map[string]int{"a": 0}, "a", "a", 0},
+		{map[string]int{"a": 0, "b": 1}, "a", "b", 1},
+		{map[string]int{"a": 0, "b": 1}, "a", "a", 0},
+		{map[string]int{"a": 0, "b": 1, "c": 2}, "b", "c", 2},
+	}
+	for i, tt := range tests {
+		s := New(len(tt.input))
+		for k, v := range tt.input {
+			s.Put(k, v)
+		}
+
+		s.Delete(tt.deleteKey)
+
+		got, _ := s.Get(tt.getKey)
+		if got != tt.want {
+			t.Errorf("%v. got %v, want %v", i, got, tt.want)
+		}
+	}
+}
+
+func TestResize(t *testing.T) {
+	tests := []struct {
+		input map[string]int
+	}{
+		{map[string]int{"a": 0}},
+		{map[string]int{"a": 0, "b": 1, "c": 2, "d": 3, "e": 4}},
+	}
+	for i, tt := range tests {
+		l := len(tt.input)
+		s := New(l)
+		for k, v := range tt.input {
+			s.Put(k, v)
+		}
+
+		s.resize(l * 2)
+
+		for k, v := range tt.input {
+			got, err := s.Get(k)
+			if err != nil {
+				t.Error(err)
+			}
+			if got != v {
+				t.Errorf("%v. got %v, want %v", i, got, v)
+			}
+		}
+	}
+}
--- DataStructures/symbolTable/separateChainingHash/separateChainingHash.go
+++ DataStructures/symbolTable/separateChainingHash/separateChainingHash.go
@@ -1,1 +1,148 @@
 package separateChainingHash
+
+import (
+	"errors"
+)
+
+type st struct {
+	len  int
+	cap  int
+	list []list
+}
+
+type list struct {
+	first *node
+}
+
+type node struct {
+	key   string
+	value int
+	next  *node
+}
+
+func (l list) get(key string) (int, error) {
+	for cur := l.first; cur != nil; cur = cur.next {
+		if key == cur.key {
+			return cur.value, nil
+		}
+	}
+	return 0, errors.New("key is not exist")
+}
+
+func (l list) contains(key string) bool {
+	_, err := l.get(key)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func (l *list) put(key string, value int) {
+	n := new(node)
+	n.key = key
+	n.value = value
+	n.next = nil
+
+	if l.first == nil {
+		l.first = n
+	} else {
+		n.next = l.first.next
+		l.first.next = n
+	}
+}
+
+func (l *list) delete(key string) {
+	cur := l.first
+	pre := l.first
+	for ; key != cur.key; cur = cur.next {
+		pre = cur
+	}
+
+	if (cur == l.first) && (pre == l.first) {
+		l.first = nil
+	} else {
+		pre.next = cur.next
+	}
+}
+
+func New(cap int) *st {
+	s := new(st)
+	s.len = 0
+	s.cap = cap
+	s.list = make([]list, s.cap)
+	return s
+}
+
+func (s st) Size() int {
+	return s.len
+}
+
+func (s st) IsEmpty() bool {
+	return s.Size() == 0
+}
+
+func (s st) Contains(key string) bool {
+	_, err := s.Get(key)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func (s st) Get(key string) (int, error) {
+	i := s.hash(key)
+	v, err := s.list[i].get(key)
+	if err != nil {
+		return 0, err
+	}
+	return v, nil
+}
+
+func (s *st) resize(cap int) {
+	newST := New(cap)
+	for i := 0; i < s.cap; i++ {
+		for cur := s.list[i].first; cur != nil; cur = cur.next {
+			newST.Put(cur.key, cur.value)
+		}
+	}
+	s.len = newST.len
+	s.cap = newST.cap
+	s.list = newST.list
+}
+
+func (s st) hash(key string) int {
+	return (s.hashCode(key) & 0x7fffffff) % s.cap
+}
+
+func (s st) hashCode(key string) int {
+	hash := 0
+	l := len(key)
+	for i := 0; i < l; i++ {
+		hash = 31*hash + int(key[i])
+	}
+	return hash
+}
+
+func (s *st) Put(key string, value int) {
+	if s.len >= (s.cap * 10) {
+		s.resize(s.cap * 2)
+	}
+
+	i := s.hash(key)
+	if !s.list[i].contains(key) {
+		s.len++
+	}
+	s.list[i].put(key, value)
+}
+
+func (s *st) Delete(key string) {
+	i := s.hash(key)
+	if s.list[i].contains(key) {
+		s.len--
+	}
+	s.list[i].delete(key)
+
+	if (s.cap > 4) && (s.len <= (s.cap * 2)) {
+		s.resize(s.cap / 2)
+	}
+}
