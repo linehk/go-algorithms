366b99f67eb81e7d0a898708841de624623f0d75
sulinehk <sulinehk@gmail.com>
2019-01-08 21:56:20 +0800

    DataStructures/graph: fix adjacencyList

--- DataStructures/graph/adjacencyList/adjacencyList_test.go
+++ DataStructures/graph/adjacencyList/adjacencyList_test.go
@@ -1,1 +1,15 @@
 package adjacencyList
+
+import (
+	"testing"
+)
+
+func TestNewEdge(t *testing.T) {
+	u := NewUndirectedGraph(10)
+	u.adj[0].put("aa", 1)
+	v, err := u.adj[0].get("aa")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(v)
+}
